cf5d584397867307b1e6dc4dfd9affe453a42e4e
mjuarezm <marcjuarez89@gmail.com>
2021-12-01 13:45:15 -0800

    add files from Zafar et al

--- disparate_impact/synthetic_data_demo/generate_synthetic_data.py
+++ disparate_impact/synthetic_data_demo/generate_synthetic_data.py
@@ -7,7 +7,7 @@ SEED = 1122334455
 seed(SEED) # set the random seed so that the random permutations can be reproduced again
 np.random.seed(SEED)
 
-def generate_synthetic_data(plot_data=False):
+def generate_synthetic_data(n_samples=1000, plot_data=False):
 
     """
         Code for generating the synthetic data.
@@ -15,7 +15,6 @@ def generate_synthetic_data(plot_data=False):
         A sensitive feature value of 0.0 means the example is considered to be in protected group (e.g., female) and 1.0 means it's in non-protected group (e.g., male).
     """
 
-    n_samples = 1000 # generate these many data points per class
     disc_factor = math.pi / 4.0 # this variable determines the initial discrimination in the data -- decraese it to generate more discrimination
 
     def gen_gaussian(mean_in, cov_in, class_label):
@@ -36,7 +35,7 @@ def generate_synthetic_data(plot_data=False):
     y = np.hstack((y1, y2))
 
     # shuffle the data
-    perm = range(0,n_samples*2)
+    perm = np.arange(0, n_samples*2).astype(int)
     shuffle(perm)
     X = X[perm]
     y = y[perm]
@@ -62,11 +61,11 @@ def generate_synthetic_data(plot_data=False):
         r = np.random.uniform() # generate a random number from 0 to 1
 
         if r < p1: # the first cluster is the positive class
-            x_control.append(1.0) # 1.0 means its male
+            x_control.append(1) # 1.0 means its male
         else:
-            x_control.append(0.0) # 0.0 -> female
+            x_control.append(0) # 0.0 -> female
 
-    x_control = np.array(x_control)
+    x_control = np.array(x_control).astype(int)
 
     """ Show the data """
     if plot_data:
@@ -75,16 +74,15 @@ def generate_synthetic_data(plot_data=False):
         y_draw = y[:num_to_draw]
         x_control_draw = x_control[:num_to_draw]
 
-        X_s_0 = x_draw[x_control_draw == 0.0]
-        X_s_1 = x_draw[x_control_draw == 1.0]
-        y_s_0 = y_draw[x_control_draw == 0.0]
-        y_s_1 = y_draw[x_control_draw == 1.0]
-        plt.scatter(X_s_0[y_s_0==1.0][:, 0], X_s_0[y_s_0==1.0][:, 1], color='green', marker='x', s=30, linewidth=1.5, label= "Prot. +ve")
-        plt.scatter(X_s_0[y_s_0==-1.0][:, 0], X_s_0[y_s_0==-1.0][:, 1], color='red', marker='x', s=30, linewidth=1.5, label = "Prot. -ve")
-        plt.scatter(X_s_1[y_s_1==1.0][:, 0], X_s_1[y_s_1==1.0][:, 1], color='green', marker='o', facecolors='none', s=30, label = "Non-prot. +ve")
-        plt.scatter(X_s_1[y_s_1==-1.0][:, 0], X_s_1[y_s_1==-1.0][:, 1], color='red', marker='o', facecolors='none', s=30, label = "Non-prot. -ve")
+        X_s_0 = x_draw[x_control_draw == 0]
+        X_s_1 = x_draw[x_control_draw == 1]
+        y_s_0 = y_draw[x_control_draw == 0]
+        y_s_1 = y_draw[x_control_draw == 1]
+        plt.scatter(X_s_0[y_s_0 ==1][:, 0], X_s_0[y_s_0 ==1][:, 1], color='green', marker='x', s=30, linewidth=1.5, label= "Prot. +ve")
+        plt.scatter(X_s_0[y_s_0==-1][:, 0], X_s_0[y_s_0==-1][:, 1], color='red', marker='x', s=30, linewidth=1.5, label = "Prot. -ve")
+        plt.scatter(X_s_1[y_s_1 ==1][:, 0], X_s_1[y_s_1 ==1][:, 1], color='green', marker='o', facecolors='none', s=30, label = "Non-prot. +ve")
+        plt.scatter(X_s_1[y_s_1==-1][:, 0], X_s_1[y_s_1==-1][:, 1], color='red', marker='o', facecolors='none', s=30, label = "Non-prot. -ve")
 
-        
         plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off') # dont need the ticks to see the data distribution
         plt.tick_params(axis='y', which='both', left='off', right='off', labelleft='off')
         plt.legend(loc=2, fontsize=15)
@@ -93,5 +91,6 @@ def generate_synthetic_data(plot_data=False):
         plt.savefig("img/data.png")
         plt.show()
 
+    c_control = x_control.copy()
     x_control = {"s1": x_control} # all the sensitive features are stored in a dictionary
-    return X,y,x_control
+    return X[c_control==1], y[c_control==1], X[c_control==0], y[c_control==0], X, y, x_control
